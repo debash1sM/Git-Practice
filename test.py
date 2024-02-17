3e6d703a9de69a218e58f71d8d014ed9b68e9ad9
“Debashis <debashis.deep205@gmail.com>
2024-02-17 16:56:36 +0530

    update file content

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 # Test commit
 file_path = "new_file.txt"
-with open(file_path, 'w') as file:
+# with open(file_path, 'w') as file:
+#     for i in range(1000):
+#         file.write("bolbo na \n")
+# print(f"File '{file_path}' created successfully.")
+
+with open(file_path, "r+") as file:
+    data = file.read()
+    file.seek(0)  # Move the cursor to the beginning of the file
     for i in range(1000):
-        file.write("bolbo na \n")
-print(f"File '{file_path}' created successfully.")
+         file.write("test input\n")
+    file.truncate() 
 
 
 
