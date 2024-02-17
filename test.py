8a5fecf42b0564bd93daa63b49eb3684e64a996f
“Debashis <debashis.deep205@gmail.com>
2024-02-17 16:22:59 +0530

    created a new file

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 # Test commit
+file_path = "new_file.txt"
+with open(file_path, 'w') as file:
+    for i in range(1000):
+        file.write("bolbo na \n")
+print(f"File '{file_path}' created successfully.")
+
+
+
